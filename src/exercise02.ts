ea0992381e54b841bc63b084d479def7e9156ba1
Jaden Patterson <pattersonjaden48@gmail.com>
2026-08-24 19:17:10 -0400

    Completed Exercise 01

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,22 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+  const complements: Record<string, string> = {
+    'A': 'U',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+  };
+
+let ret = "";
+
+for (const c of dna) {
+  if (c in complements){
+    ret += complements[c];
+  }
+  else {
+    throw new TypeError('Invalid nucleotide: ${c}');
+  }
 }
+
+return ret;
+}
+
