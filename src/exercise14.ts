a76c5e5abb252a7020e1dd9001ee121d777c2091
Jaden Patterson <pattersonjaden48@gmail.com>
2026-09-02 15:33:12 -0400

    Completed Exercise 14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,17 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const requests = postIds.map(async (id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+    const post = (await response.json()) as PostItem;
+
+    return post;
+  });
+
+  return Promise.all(requests);
 }
