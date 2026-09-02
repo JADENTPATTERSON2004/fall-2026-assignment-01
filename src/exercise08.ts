82307b59fd53a0eebc5e1b7cc49a41a12d6bc268
Jaden Patterson <pattersonjaden48@gmail.com>
2026-09-02 00:11:25 -0400

    Completed Exercise 08

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -3,11 +3,29 @@ export type EventMap = {
   shutdown: number;
 };
 
+type EventHandlers<T extends EventMap> = {
+  [K in keyof T]?: Array<(data: T[K]) => void>;
+};
+
 export class SimpleEventEmitter<T extends EventMap> {
+  private handlers: EventHandlers<T> = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.handlers[eventName]) {
+      this.handlers[eventName] = [];
+    }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+    this.handlers[eventName]!.push(callback);
+  }
+
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const callbacks = this.handlers[eventName];
+
+    if (callbacks) {
+      callbacks.forEach((callback) => callback(data));
+    }
+  }
 }
