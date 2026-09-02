33cc2e133f5f6233628b9d5b516a42719ae43286
Jaden Patterson <pattersonjaden48@gmail.com>
2026-09-02 15:25:35 -0400

    Completed Exercise 13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,22 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+  
+    if (!response.ok) {
+      throw new Error("Failed to fetch todo");
+    }
+
+    const todo = (await response.json()) as TodoItem;
+
+    return todo;
+  } catch {
+    return null;
+  }
 }
